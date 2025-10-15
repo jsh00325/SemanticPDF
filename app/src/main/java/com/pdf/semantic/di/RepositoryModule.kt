242029779415b072e0abc2f73374bf8ea2ca2152
정수현 <jsh00325@naver.com>
2025-10-15 23:47:01 +0900

    Feat: POC 화면 구현 및 UI 구성 (#17)
    * Feat: 기능 검증을 위한 PoC 액티비티 추가
    
    * Feat: POC를 위한 화면 및 상태 초기 구현
    
    * Feat: 에러 상태 UI를 위한 상태 클래스 추가
    
    * Feat: 로딩 인디케이터와 PDF 슬라이드 정보 UI 구현
    
    * Feat: PDF 문서를 파싱하여 보여주는 UI 구현
    
    * Design: 슬라이드 정보에 유사도 표시
    
    * Feat: EmbeddingRepository 의존성 주입 설정
    
    * Feat: 검색창 UI 구현 및 로딩 상태 Ui 상태 통일
    
    * Feat: 검색창 UI 구현 및 로딩 상태 Ui 상태 통일

--- app/src/main/java/com/pdf/semantic/di/RepositoryModule.kt
+++ app/src/main/java/com/pdf/semantic/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.pdf.semantic.di
 
+import com.pdf.semantic.data.repositoryImpl.EmbeddingRepositoryImpl
 import com.pdf.semantic.data.repositoryImpl.PdfRepositoryImpl
+import com.pdf.semantic.domain.repository.EmbeddingRepository
 import com.pdf.semantic.domain.repository.PdfRepository
 import dagger.Binds
 import dagger.Module
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindPdfRepository(pdfRepositoryImpl: PdfRepositoryImpl): PdfRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindEmbeddingRepository(embeddingRepositoryImpl: EmbeddingRepositoryImpl): EmbeddingRepository
 }
---
+++ app/src/main/java/com/pdf/semantic/presentation/poc/PocActivity.kt
@@ -0,0 +1,21 @@
+package com.pdf.semantic.presentation.poc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.pdf.semantic.ui.theme.SemanticPDFTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PocActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SemanticPDFTheme {
+                PocScreen()
+            }
+        }
+    }
+}

e3f6caa0ab05a8b9be57f0a3574310c0a14f8ceb
Jeong Suhyeon <jsh00325@naver.com>
2025-10-29 00:42:52 +0900

    Feat: ObjectBox 초기 설정 및 DI 구현

---
+++ app/src/main/java/com/pdf/semantic/di/DatabaseModule.kt
@@ -0,0 +1,21 @@
+package com.pdf.semantic.di
+
+import android.content.Context
+import com.pdf.semantic.data.entity.MyObjectBox
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.objectbox.BoxStore
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context,
+    ): BoxStore = MyObjectBox.builder().androidContext(context).build()
+}

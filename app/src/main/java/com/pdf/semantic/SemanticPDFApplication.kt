5768d16f31fe2009f15ed4c2167e5e05023a6f0f
Jeong Suhyeon <jsh00325@naver.com>
2025-10-30 17:06:04 +0900

    Refactor: Firebase App Check 관련 코드 삭제

--- app/src/main/java/com/pdf/semantic/SemanticPDFApplication.kt
+++ app/src/main/java/com/pdf/semantic/SemanticPDFApplication.kt
@@ -1,19 +1,7 @@
 package com.pdf.semantic
 
 import android.app.Application
-import com.google.firebase.Firebase
-import com.google.firebase.appcheck.appCheck
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
-import com.google.firebase.initialize
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SemanticPDFApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Firebase.initialize(context = this)
-        Firebase.appCheck.installAppCheckProviderFactory(
-            PlayIntegrityAppCheckProviderFactory.getInstance(),
-        )
-    }
-}
+class SemanticPDFApplication : Application()

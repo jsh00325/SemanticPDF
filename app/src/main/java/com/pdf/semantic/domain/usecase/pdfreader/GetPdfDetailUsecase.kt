baad25d78e530016598e0dc2f8094bfc3baf94b8
Jiphago <jjj8785@gmail.com>
2025-10-31 19:52:05 +0900

    Feat: GetPdfDetailUsecase 추가

--- app/src/main/java/com/pdf/semantic/domain/usecase/pdfreader/GetPdfDetailUsecase.kt
+++ app/src/main/java/com/pdf/semantic/domain/usecase/pdfreader/GetPdfDetailUsecase.kt
@@ -1,11 +1,14 @@
 package com.pdf.semantic.domain.usecase.pdfreader
 
+import com.pdf.semantic.domain.model.PdfItem
+import com.pdf.semantic.domain.repository.PdfMetadataRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetPdfDetailUsecase
     @Inject
-    constructor() {
-        suspend operator fun invoke() {
-            // TODO: 추후 구현 예정
-        }
+    constructor(
+        private val pdfMetadataRepository: PdfMetadataRepository,
+    ) {
+        suspend operator fun invoke(pdfId: Long): Flow<PdfItem> = pdfMetadataRepository.observePdfMetadata(pdfId)
     }

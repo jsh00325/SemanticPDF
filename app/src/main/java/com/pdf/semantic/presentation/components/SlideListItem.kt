d0597cb91e97fc4b360625e199f92a750f8e61e8
Jiphago <jjj8785@gmail.com>
2025-11-26 03:04:55 +0900

    Style: 슬라이드 로딩 시 배경색을 흰색으로 변경

--- app/src/main/java/com/pdf/semantic/presentation/components/SlideListItem.kt
+++ app/src/main/java/com/pdf/semantic/presentation/components/SlideListItem.kt
@@ -1,17 +1,18 @@
 package com.pdf.semantic.presentation.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -55,7 +56,14 @@ fun SlideListItem(
                         contentScale = ContentScale.Crop,
                     )
                 } else {
-                    CircularProgressIndicator()
+                    // CircularProgressIndicator()
+                    Box(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .aspectRatio(1f / 1.414f)
+                                .background(Color.White),
+                    )
                 }
 
                 Text(
